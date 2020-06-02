8af23861c234b4ca19baa6c82892dc279bbee69a
thisisthanh97 <thisisthanh97@gmail.com>
2020-06-02 15:53:51 +0700

    first commit

---
+++ commands.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import os
+import sys
+import subprocess
+import shutil
+
+# subprocess.Popen('sudo apt-get update')
+# subprocess.Popen('sudo apt install python')
+os.system("echo 'Hello'")
+os.system('ls -l')
